c23d941d4b9733d3439457eec1f75a805b066a93
Ian <ian@invacto.com>
2021-01-10 22:59:05 +0100

    added value plugin

---
+++ src/plugins/raw_value/ValuePlugin.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace invacto\SimpleCMS\plugins\raw_value;
+
+
+use Exception;
+use invacto\SimpleCMS\plugins\Plugin;
+use invacto\SimpleCMS\plugins\PluginContext;
+
+class ValuePlugin extends Plugin
+{
+
+	public function setup()
+	{
+		/**
+		 * Params:
+		 * 0: Name (snippet name)
+		 * 1: DefaultValue
+		 */
+		$this->addTemplateFunction("value", function (PluginContext $pluginContext, $params = []) {
+			if (sizeof($params) < 1) {
+				throw new Exception("'value' function needs at least 1 parameter (the snippet name)");
+			}
+			$name = $params[0];
+			$defaultValue = (isset($params[1]) ? $params[1] : "");
+			return $pluginContext->findSnippet($name, $defaultValue);
+		});
+	}
+
+}
